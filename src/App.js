11d547b3712c86ecde99940fe1dce94b92d2d4a7
rsathiya125@gmail.com <rsathiya125@gmail.com>
2024-10-15 02:31:55 -0700

    updated

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+    
+        
+        <h1>Sathiya R</h1>
+        <hr/>
+        <p>Mobile no:8940162884 Email id:rsathiya125@gmail.com<div>Address:Sirkazhi 609116</div></p>
+        <hr/>
+        <p><h3>Summary</h3></p>
+        <h4> I am a innovative college student.Trough Hand on project,internship collaboration and using in Gitup and java my Goal on learning and explor my skill and knowledge to make a meaningfull impack in the world</h4>
+        <hr/>
+        <p><h3>Skill</h3></p>
+        <h4>*Github*Git*HTML*Team work and collaboration</h4>
+        <hr/>
+        <p><h3>Education</h3></p>
+        <h5>Bachelor of computer science and engineering,<div>vetri vinayaha college of engineering and technology,thottiam.</div>
+        </h5>
+        year:2024
+        <hr/>
+        <p><h3>languages</h3></p>
+        <h5>English First language</h5>
+      
+    </div>
+  );
+}
+
+export default App;
